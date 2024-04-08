c009d0f150e3ecad9678d897c958f0c53c748e7c
Lea TALLEC <lea.tallec2023@campus-eni.fr>
2024-04-08 14:14:28 +0200

    Create class SoldItem and CollectParcel

---
+++ src/main/java/fr/eni/tp/enienchere/bo/CollectParcel.java
@@ -0,0 +1,79 @@
+package fr.eni.tp.enienchere.bo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class CollectParcel {
+    private String street;
+    private String zipCode;
+    private String city;
+
+    private List<SoldItem> soldItems = new ArrayList<>();
+
+    public CollectParcel() {}
+
+    public CollectParcel(String street, String zipCode, String city) {
+        this.street = street;
+        this.zipCode = zipCode;
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public List<SoldItem> getSoldItems() {
+        return soldItems;
+    }
+
+    public void addSoldItem(SoldItem soldItem) {
+        this.soldItems.add(soldItem);
+    }
+
+    public void setSoldItems(List<SoldItem> soldItems) {
+        this.soldItems = soldItems;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CollectParcel that)) return false;
+        return Objects.equals(street, that.street) && Objects.equals(zipCode, that.zipCode) && Objects.equals(city, that.city) && Objects.equals(soldItems, that.soldItems);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(street, zipCode, city, soldItems);
+    }
+
+    @Override
+    public String toString() {
+        return "CollectParcel{" +
+                "street='" + street + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", city='" + city + '\'' +
+                ", soldItems=" + soldItems +
+                '}';
+    }
+}
--- src/main/java/fr/eni/tp/enienchere/bo/User.java
+++ src/main/java/fr/eni/tp/enienchere/bo/User.java
@@ -13,7 +13,7 @@ public class User {
     private String email;
     private String phone;
     private String street;
-    private long zipCode;
+    private String zipCode;
     private String password;
     private long credit;
     private boolean admin;
@@ -91,11 +91,11 @@ public class User {
         this.street = street;
     }
 
-    public long getZipCode() {
+    public String getZipCode() {
         return zipCode;
     }
 
-    public void setZipCode(long zipCode) {
+    public void setZipCode(String zipCode) {
         this.zipCode = zipCode;
     }
 
