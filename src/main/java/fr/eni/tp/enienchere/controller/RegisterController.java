3cc9a280075bf38195216c3a116d046bd2738199
Lea TALLEC <lea.tallec2023@campus-eni.fr>
2024-04-09 13:24:26 +0200

    Front register

--- src/main/java/fr/eni/tp/enienchere/controller/RegisterController.java
+++ src/main/java/fr/eni/tp/enienchere/controller/RegisterController.java
@@ -1,9 +1,11 @@
 package fr.eni.tp.enienchere.controller;
 
 import fr.eni.tp.enienchere.bo.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class RegisterController {
     @GetMapping("/inscription")
     public String index(Model model) {
