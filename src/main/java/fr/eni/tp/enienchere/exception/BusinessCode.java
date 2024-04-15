0232efa3e3027111e14619e067c123b93db18dd1
Samar-al <samar.alkhalilchebaye@gmail.com>
2024-04-15 10:49:11 +0200

    message error file upload

--- src/main/java/fr/eni/tp/enienchere/exception/BusinessCode.java
+++ src/main/java/fr/eni/tp/enienchere/exception/BusinessCode.java
@@ -4,6 +4,7 @@ public class BusinessCode {
     public static final String VALIDATION_CONFIRMATION_PASSWORD = "validation.confirmation.password";
     public static final String NOT_ENOUGH_CREDIT = "validation.credit";
     public static final String NOT_ENOUGH_BID_AMOUNT = "validation.bid.amount";
+    public static final String BID_HAS_ENDED = "bid.has.ended";
 
 
 
